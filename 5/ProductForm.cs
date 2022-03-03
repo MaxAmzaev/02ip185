c53ceef57b5ac3900212c28dca9332a4457a79c7
amzaev_02ip185 <02ip185@git.scc>
2022-03-03 21:56:15 +0300

    4-й день

---
+++ 5/ProductForm.cs
@@ -0,0 +1,25 @@
+namespace vosmerka
+{
+    public partial class ProductForm : Form
+    {
+        public ProductForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
